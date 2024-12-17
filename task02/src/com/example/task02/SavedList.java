efa52ff5673baa4252f2e52a423bb98f22edc159
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-12-17 23:31:03 +0500

    2 задание

--- task02/src/com/example/task02/SavedList.java
+++ task02/src/com/example/task02/SavedList.java
@@ -1,35 +1,86 @@
 package com.example.task02;
 
-import java.io.File;
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.AbstractList;
+import java.util.ArrayList;
 
-public class SavedList<E extends Serializable> extends AbstractList<E> {
+public class SavedList<E extends Serializable> extends AbstractList<E>
+{
+    private final ArrayList<E> list = new ArrayList<>();
+    private final File file;
 
-    public SavedList(File file) {
+    public SavedList(File file)
+    {
+        this.file = file;
+        loadFromFile();
+    }
+
+    private void loadFromFile()
+    {
+        if (!file.exists())
+        {
+            return;
+        }
+
+        try (ObjectInputStream stream = new ObjectInputStream(Files.newInputStream(file.toPath())))
+        {
+            @SuppressWarnings("unchecked") // <- Чтобы глаза не мазолило
+            ArrayList<E> loadedList = (ArrayList<E>) stream.readObject();
+            list.addAll(loadedList);
+        }
+
+        catch (IOException | ClassNotFoundException e)
+        {
+            throw new RuntimeException("Error when loading data from file", e);
+        }
+    }
+
+    private void saveToFile()
+    {
+        try (ObjectOutputStream stream = new ObjectOutputStream(Files.newOutputStream(file.toPath())))
+        {
+            stream.writeObject(list);
+        }
+
+        catch (IOException e)
+        {
+            throw new RuntimeException("Error when saving data to file", e);
+        }
     }
 
     @Override
-    public E get(int index) {
-        return null;
+    public E get(int index)
+    {
+        return list.get(index);
     }
 
     @Override
-    public E set(int index, E element) {
-        return null;
+    public E set(int index, E element)
+    {
+        E oldElement = list.set(index, element);
+        saveToFile();
+        return oldElement;
     }
 
     @Override
-    public int size() {
-        return 0;
+    public int size()
+    {
+        return list.size();
     }
 
     @Override
-    public void add(int index, E element) {
+    public void add(int index, E element)
+    {
+        list.add(index, element);
+        saveToFile();
     }
 
     @Override
-    public E remove(int index) {
-        return null;
+    public E remove(int index)
+    {
+        E removedElement = list.remove(index);
+        saveToFile();
+        return removedElement;
     }
 }
