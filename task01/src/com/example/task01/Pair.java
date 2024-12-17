7855d260013653b8da002b4889dd9c5d6760a49b
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-12-17 23:30:46 +0500

    1 задание

--- task01/src/com/example/task01/Pair.java
+++ task01/src/com/example/task01/Pair.java
@@ -1,5 +1,63 @@
 package com.example.task01;
 
-public class Pair {
-    // TODO напишите реализацию
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+public class Pair<A, B>
+{
+    private final A first;
+    private final B second;
+
+    private Pair(A first, B last)
+    {
+        this.first = first;
+        this.second = last;
+    }
+
+    public static <A, B> Pair<A, B> of(A first, B second)
+    {
+        return new Pair<>(first, second);
+    }
+
+    public A getFirst()
+    {
+        return first;
+    }
+
+    public B getSecond()
+    {
+        return second;
+    }
+
+    public void ifPresent(BiConsumer<? super A, ? super B> action)
+    {
+        if (first != null && second != null)
+        {
+            action.accept(first, second);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass())
+        {
+            return false;
+        }
+
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
+
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(first, second);
+    }
 }
