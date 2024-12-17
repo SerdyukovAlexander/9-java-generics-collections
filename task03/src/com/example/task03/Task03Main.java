bb7f31f126440cf37dbb508299b0ded87d6d847f
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-12-17 23:31:09 +0500

    3 задание

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,24 +1,49 @@
 package com.example.task03;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Task03Main {
+public class Task03Main
+{
 
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) throws IOException
+    {
         List<Set<String>> anagrams = findAnagrams(new FileInputStream("task03/resources/singular.txt"), Charset.forName("windows-1251"));
-        for (Set<String> anagram : anagrams) {
+        for (Set<String> anagram : anagrams)
+        {
             System.out.println(anagram);
         }
 
     }
 
-    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset) {
-        return null;
+    public static List<Set<String>> findAnagrams(InputStream inputStream, Charset charset)
+    {
+        Map<String, Set<String>> result = new TreeMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset)))
+        {
+            reader.lines().map(String::toLowerCase)
+                    .filter(word -> word.matches("[а-яё]+") && word.length() >= 3)
+
+                    .forEach(word ->
+                    {
+                        String sortedChars = word.chars().sorted()
+                                .mapToObj(c -> String.valueOf((char) c))
+                                .collect(Collectors.joining());
+
+                        result.computeIfAbsent(sortedChars, key -> new TreeSet<>()).add(word);
+                    });
+        }
+
+        catch (IOException e)
+        {
+            throw new RuntimeException("Somethings wrong", e);
+        }
+
+        return result.values().stream()
+                .filter(group -> group.size() >= 2)
+                .collect(Collectors.toList());
     }
 }
